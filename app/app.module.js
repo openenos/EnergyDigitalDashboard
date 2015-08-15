9852ac357d02e0b9842fe0e94569c3a38ad16b54
Balaraju <vankalabalaraju@gmail.com>
2015-08-16 04:45:41 +0530

    Balaraju - Addded Routes

--- app/app.module.js
+++ app/app.module.js
@@ -1,4 +1,4 @@
-angular.module('enos', ['enos.controllers', 'googlechart']);
+angular.module('enos', ['enos.controllers', 'googlechart', 'ngRoute']);
 angular.module('enos.controllers', ['enos.services', 'enos.factories']);
 angular.module('enos.services', []);
 angular.module('enos.factories', []);
