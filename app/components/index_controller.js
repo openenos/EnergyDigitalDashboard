71efe78d899cb804ed81c18792abeda579631f30
Balaraju <vankalabalaraju@gmail.com>
2015-08-15 18:53:14 +0530

    Balaraju - Added Fonts

--- app/components/index_controller.js
+++ app/components/index_controller.js
@@ -2,25 +2,56 @@ angular.module('enos.controllers')
 	.controller('IndexController', ['$scope', function ($scope) {
 		$scope.name = "balaraju"
               
-		var chart1 = {};
-	    chart1.type = "Gauge";
-	    chart1.cssStyle = "height:200px; width:300px;";
-	    chart1.data = [ ['Label', 'Value'],
-          ['Memory', 80],
-          ['CPU', 55],
-          ['Network', 68]];
-	    chart1.options = {
-	    	 "width": 400, 
-	    	 "height": 120,
-          "redFrom": 90, "redTo": 100,
-          "yellowFrom":75, "yellowTo": 90,
-         "minorTicks": 5
-	       
-	    };
+		 var chart1 = {};
+    chart1.type = "ColumnChart";
+    chart1.cssStyle = "height:200px; width:300px;";
+    chart1.data = {"cols": [
+        {id: "month", label: "Month", type: "string"},
+        {id: "laptop-id", label: "Laptop", type: "number"},
+        {id: "desktop-id", label: "Desktop", type: "number"},
+        {id: "server-id", label: "Server", type: "number"},
+        {id: "cost-id", label: "Shipping", type: "number"}
+    ], "rows": [
+        {c: [
+            {v: "January"},
+            {v: 19, f: "42 items"},
+            {v: 12, f: "Ony 12 items"},
+            {v: 7, f: "7 servers"},
+            {v: 4}
+        ]},
+        {c: [
+            {v: "February"},
+            {v: 13},
+            {v: 1, f: "1 unit (Out of stock this month)"},
+            {v: 12},
+            {v: 2}
+        ]},
+        {c: [
+            {v: "March"},
+            {v: 24},
+            {v: 0},
+            {v: 11},
+            {v: 6}
 
-	    chart1.formatters = {};
+        ]}
+    ]};
 
-	    $scope.chart2 = chart1;
+    chart1.options = {
+        "title": "Sales per month",
+        "isStacked": "true",
+        "fill": 20,
+        "displayExactValues": true,
+        "vAxis": {
+            "title": "Sales unit", "gridlines": {"count": 6}
+        },
+        "hAxis": {
+            "title": "Date"
+        }
+    };
+
+    chart1.formatters = {};
+
+    $scope.chart = chart1;
     }]);
 
 
